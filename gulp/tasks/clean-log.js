bca3c2adba2c7399f27aa23f0c2db57231f4aa23
naoki <duvingtdeux@gmail.com>
2019-11-05 20:08:03 +0900

    fix: files to directories

--- gulp/tasks/clean-log.js
+++ gulp/tasks/clean-log.js
@@ -1,3 +1,7 @@
-var 
+var config = require("../config.js");
 var gulp = require("gulp");
+var del = require("del");
 
+gulp.task("clean-log", (done)=>{
+  return del("./**/*", { cwd: config.path.log })
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require("gulp");
+var load = require("require-dir");
+
+load("./gulp/tasks", { recurse: true });
 
 gulp.task("default", (done)=>{
   console.log(process.env.NODE_ENV);
